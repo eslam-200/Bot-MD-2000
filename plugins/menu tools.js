a0c3b7bf2d7228b1cdab98da5797496b56564912
beko0998 <beko0998be@gmail.com>
2024-09-07 14:54:20 +0200

    Create menu tools.js

---
+++ plugins/menu tools.js
@@ -0,0 +1,39 @@
+let handler = async (m, { conn }) => {
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let videoUrl = 'https://telegra.ph/file/3782975aafc54e78e9d0c.mp4'
+  let { name } = global.db.data.users[who]
+  m.react('⚙️')
+let str = `                  *┃━━━━⬣Astro-𝙱𝙾𝚃⬣━━━━┃*
+*【..≼قــســم الادوات≽..】*
+                   *⋄━───═◞⬪⚙️⬪◟═───━⋄*
+
+
+*『 ⚙️ 』 ⇇╎لرابط』*
+*『 ⚙️ 』 ⇇╎باركود』*
+*『 ⚙️ 』 ⇇╎ترجمه』*
+*『 ⚙️ 』 ⇇╎فضح』*
+*『 ⚙️ 』 ⇇╎تفريغ』*
+*『 ⚙️ 』 ⇇╎فحص』*
+*『 ⚙️ 』 ⇇╎تكرار』*
+*『 ⚙️ 』 ⇇╎بريد』*
+*『 ⚙️ 』 ⇇╎قص』*
+*『 ⚙️ 』 ⇇╎احسب』*
+*『 ⚙️ 』 ⇇╎جوده』*
+*『 ⚙️ 』 ⇇╎واتس』*
+*『 ⚙️ 』 ⇇╎طلوب』*
+*『 ⚙️ 』 ⇇╎انسخ』*
+*『 ⚙️ 』 ⇇╎بينغ』*
+                    *┃━━━━⬣Astro-𝙱𝙾𝚃⬣━━━━┃*
+`
+  conn.sendMessage(m.chat, {
+           video: { url: videoUrl }, caption: str,
+     mentions: [m.sender,global.conn.user.jid],
+     gifPlayback: true,gifAttribution: 0
+       }, { quoted: m });
+   };
+
+handler.help = ['main']
+handler.tags = ['group']
+handler.command = ['الاداوات']
+
+export default handler
