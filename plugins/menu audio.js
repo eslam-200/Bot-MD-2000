0776ee0c69959d7e3e7c9e9aa347a5e09dddcecb
beko0998 <beko0998be@gmail.com>
2024-09-07 14:46:17 +0200

    Create menu audio.js

---
+++ plugins/menu audio.js
@@ -0,0 +1,33 @@
+let handler = async (m, { conn }) => {
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let videoUrl = 'https://telegra.ph/file/3782975aafc54e78e9d0c.mp4'
+  let { name } = global.db.data.users[who]
+  m.react('🔊')
+let str = `                  *┃━━━━⬣Astro-𝙱𝙾𝚃⬣━━━━┃*
+*【..≼قــسم الصــوتيـــات≽..】*
+                   *⋄━───═◞⬪🎧⬪◟═───━⋄*
+*『 🎧 』 ⇇╎عميق 』*
+*『 🎧 』 ⇇╎منفوخ 』*
+*『 🎧 』 ⇇╎تخين 』*
+*『 🎧 』 ⇇╎صاخب 』*
+*『 🎧 』 ⇇╎سريع 』*
+*『 🎧 』 ⇇╎رفيع 』*
+*『 🎧 』 ⇇╎روبوت 』*
+*『 🎧 』 ⇇╎بطئ 』*
+*『 🎧 』 ⇇╎ناعم 』*
+*『 🎧 』 ⇇╎سنجاب 』*
+*『 🎧 』 ⇇╎مكس 』*
+                    *┃━━━━⬣Astro-𝙱𝙾𝚃⬣━━━━┃*
+`
+  conn.sendMessage(m.chat, {
+           video: { url: videoUrl }, caption: str,
+     mentions: [m.sender,global.conn.user.jid],
+     gifPlayback: true,gifAttribution: 0
+       }, { quoted: m });
+   };
+
+handler.help = ['main']
+handler.tags = ['group']
+handler.command = ['الاصوات']
+
+export default handler
