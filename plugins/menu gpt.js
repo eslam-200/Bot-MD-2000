1a75485e0aa86dbe56e618da765cf4a69e008506
beko0998 <beko0998be@gmail.com>
2024-09-07 14:49:30 +0200

    Create menu gpt.js

---
+++ plugins/menu gpt.js
@@ -0,0 +1,33 @@
+let handler = async (m, { conn }) => {
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let videoUrl = 'https://telegra.ph/file/3782975aafc54e78e9d0c.mp4'
+  let { name } = global.db.data.users[who]
+  m.react('🤖')
+let str = `                  *┃━━━━⬣Astro-𝙱𝙾𝚃⬣━━━━┃*
+*【..≼قــســم الذكاء الاصطناعي≽..】*
+                   *⋄━───═◞⬪👨🏻‍💻⬪◟═───━⋄*
+*『 👨🏻‍💻 』 ⇇╎بارد』*
+*『 👨🏻‍💻 』 ⇇╎تحدث』*
+*『 👨🏻‍💻 』 ⇇╎بوت』*
+*『 👨🏻‍💻 』 ⇇╎سيري』*
+*『 👨🏻‍💻 』 ⇇╎شوف』*
+*『 👨🏻‍💻 』 ⇇╎لكرتون』*
+*『 👨🏻‍💻 』 ⇇╎لانمي』*
+*『 👨🏻‍💻 』 ⇇╎ارسم』*
+*『 👨🏻‍💻 』 ⇇╎مارو』*
+*『 👨🏻‍💻 』 ⇇╎تست』*
+*『 👨🏻‍💻 』 ⇇╎سيلفي』*
+                    *┃━━━━⬣Astro-𝙱𝙾𝚃⬣━━━━┃*
+`
+  conn.sendMessage(m.chat, {
+           video: { url: videoUrl }, caption: str,
+     mentions: [m.sender,global.conn.user.jid],
+     gifPlayback: true,gifAttribution: 0
+       }, { quoted: m });
+   };
+
+handler.help = ['main']
+handler.tags = ['group']
+handler.command = ['ذكاءاصطناعي']
+
+export default handler
