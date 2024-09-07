d40525693be2992d2d67ecacf0f2735333e688dc
beko0998 <beko0998be@gmail.com>
2024-09-07 14:52:45 +0200

    Create menu qran.js

---
+++ plugins/menu qran.js
@@ -0,0 +1,29 @@
+let handler = async (m, { conn }) => {
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let videoUrl = 'https://telegra.ph/file/3782975aafc54e78e9d0c.mp4'
+  let { name } = global.db.data.users[who]
+  m.react('🕋')
+let str = `                  *┃━━━━⬣Astro-𝙱𝙾𝚃⬣━━━━┃*
+*【..≼قــســم الدين≽..】*
+                   *⋄━───═◞⬪🕋⬪◟═───━⋄*
+*『 🕋 』 ⇇╎ايه-الكرسي』*
+*『 🕋 』 ⇇╎الله』*
+*『 🕋 』 ⇇╎قران』*
+*『 🕋 』 ⇇╎سوره』*
+*『 🕋 』 ⇇╎سور』*
+*『 🕋 』 ⇇╎حديث』*
+*『 🕋 』 ⇇╎ذكر』*
+                    *┃━━━━⬣Astro-𝙱𝙾𝚃⬣━━━━┃*
+`
+  conn.sendMessage(m.chat, {
+           video: { url: videoUrl }, caption: str,
+     mentions: [m.sender,global.conn.user.jid],
+     gifPlayback: true,gifAttribution: 0
+       }, { quoted: m });
+   };
+
+handler.help = ['main']
+handler.tags = ['group']
+handler.command = ['الدين']
+
+export default handler
