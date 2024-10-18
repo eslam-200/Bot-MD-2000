2165d3c101b848d2e1c4972dac4b36d90c35161f
eslam-200 <eslam2004cgg@gmail.com>
2024-10-18 23:58:42 +0300

    Update رد.js

--- plugins/رد.js
+++ plugins/رد.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `*قــلــب اسكونر مـن جـوا 👀⚡*`;
-await conn.sendMessage(m.chat, { react: { text: '🙌', key: m.key } })
-    conn.sendFile(m.chat, 'https://qu.ax/rovX.jpg', 'image.jpg', message, m);
+    let message = `*قــلــب حموو مـن جـوا ❣️*`;
+await conn.sendMessage(m.chat, { react: { text: '❣️', key: m.key } })
+    conn.sendFile(m.chat, 'https://qu.ax/ZNOlp.jpg', 'image.jpg', message, m);
 };
 
 handler.customPrefix = /^اسكونر$/i;
