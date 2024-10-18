12a5c16f82d6f0c9eefc7911b061bb011f123d2d
eslam-200 <eslam2004cgg@gmail.com>
2024-10-18 23:59:43 +0300

    Update رد3.js

--- plugins/رد3.js
+++ plugins/رد3.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = '*مــنــور يـا قــلــب اســكــونــر لــي الـاوامــر اكــتــب (.اوامر) ⚡️*';
-await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
-    conn.sendFile(m.chat, 'https://qu.ax/rovX.jpg', 'image.jpg', message, m);
+    let message = '*مــنــور يـا قــلــب حموو لــي الـاوامــر اكــتــب (.اوامر) ❣️*';
+await conn.sendMessage(m.chat, { react: { text: '❣️', key: m.key } })
+    conn.sendFile(m.chat, 'https://qu.ax/ZNOlp.jpg', 'image.jpg', message, m);
 };
 
 handler.customPrefix = /^بوت|.بوت$/i;
